67cd0e40093b083833d43beab89adc1574f67171
anilkumar-puli <anilkumar@zelarsoft.com>
2021-05-07 13:07:07 +0530

    new commit

--- components/frontend.sh
+++ components/frontend.sh
@@ -12,8 +12,8 @@ DOWNLOAD_COMPONENT
 
 
 Head "Unzip Downloaded Archive"
-#unzip -o /tmp/frontend.zip &>>$LOG && cd /var/www/html && rm -rf /var/www/html/*  && mv /tmp/frontend-main/* . &&  rm -rf frontend-main README.md
-#Stat $?
+unzip -o /tmp/frontend.zip &>>$LOG && cd /var/www/html && rm -rf /var/www/html/*  && mv /tmp/frontend-main/* . &&  rm -rf frontend-main README.md
+Stat $?
 
 Head "Update Nginx Configuration"
 sed -i -e "s/var/www/html/todo/frontend/dist /var/www/html" /etc/nginx/sites-available/default
