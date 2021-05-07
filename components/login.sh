4d2f59481012af517e4c294a3c0539b6b41bccbd
anilkumar-puli <anilkumar@zelarsoft.com>
2021-05-07 22:32:46 +0530

    hello

--- components/login.sh
+++ components/login.sh
@@ -8,6 +8,8 @@ Head "Adding login User"
 id loginuser &>>$LOG
 if [ $? -ne 0 ]; then
   useradd -m -s /bin/bash loginuser
+  wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+  export PATH=$PATH:/usr/local/go/bin
   Stat $?
 fi
 
