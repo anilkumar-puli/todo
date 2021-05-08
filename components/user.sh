e2bafa9cb2df34c25605c77827ca530792ee9d92
anilkumar-puli <anilkumar@zelarsoft.com>
2021-05-08 19:22:59 +0530

    new commit

--- components/user.sh
+++ components/user.sh
@@ -4,30 +4,23 @@ source components/common.sh
 
 OS_PREREQ
 
-Head "Install Maven"
+Head "Install Maven and java"
+apt install openjdk-8-jdk -y &>>$LOG
 apt install maven -y &>>$LOG
+mvn package &>>$LOG
 Stat $?
-
-
-Head "Adding todoapp User"
-id todoapp &>>$LOG
-if [ $? -ne 0 ]; then
-  useradd -m -s /bin/bash todoapp
-  Stat $?
-fi
 
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/todoapp && rm -rf todoapp
- && unzip -o /tmp/todo.zip &>>$LOG && mv todo-main todo && cd /home/todoapp/todo &&  mvn clean package  &>>$LOG && chown todoapp:todoapp /home/todoapp -R && mv target/shipping-1.0.jar shipping.jar  &>>$LOG
+cd /home/ubuntu && rm -rf user && unzip -o /tmp/user.zip &>>$LOG && mv user-main user && cd /home/ubuntu/user &&  cd /home/ubuntu/user &&  mvn clean package  &>>$LOG && chown ubuntu:ubuntu /home/ubuntu -R && mv target/user-1.0.jar user.jar  &>>$LOG
 Stat $?
 
 Head "Update EndPoints in Service File"
-sed -i -e "s/CARTENDPOINT/cart.zsdevops01.online/" -e "s/DBHOST/mysql.zsdevops01.online/" /home/roboshop/shipping/systemd.service
+sed -i -e "s/user_endpoint/login.anilzs.ml/" /home/ubuntu/user/systemd.service
 Stat $?
 
 
 Head "Setup SystemD Service"
-mv /home/todoapp/todo/systemd.service /etc/systemd/system/todo.service && systemctl daemon-reload && systemctl start todo && systemctl enable todo &>>$LOG
+mv /home/ubuntu/user/systemd.service /etc/systemd/system/user.service && systemctl daemon-reload && systemctl start user && systemctl enable user &>>$LOG
 Stat $?
